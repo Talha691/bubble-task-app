8c0467f32428b3377b46750fea2f52e47d8a6242
Talha Saeed <126463838+Talha691@users.noreply.github.com>
2024-03-16 04:07:54 +0500

    quick fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,27 +8,58 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  public tasksList: any[]= [];
   public taskForm = new FormGroup({
-    title: new FormControl(''),
-    todo: new FormControl(''),
-    description: new FormControl('')
-  })
+    title: new FormControl('', Validators.required),
+    todo: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required)
+  });
 
-  public tasks: any = [
-    {number: 1, title: 'Task Title', todo: "todo", description: 'Description'}
-  ]
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
 
-  constructor(private fb: FormBuilder) { }
-
-  ngOnInit(): void {}
+  ngOnInit(): void 
+  {
+    this.getTasks();
+  }
 
   onSubmit() {
     if (this.taskForm.valid) {
-      // Logic to handle form submission
-      console.log(this.taskForm.value);
+      const payload = {
+        title: this.taskForm.get('title')?.value,
+        todo: this.taskForm.get('todo')?.value,
+        description: this.taskForm.get('description')?.value
+      };
+      
+      this.http.post('http://localhost:3000/create-task', payload)
+      .subscribe(
+        (response: any) => {
+          if (response && response.status === 'success')
+          {
+            this.getTasks()
+          }
+          console.log('API Response:', response);
+        },
+        error => {
+          console.error('API Error:', error);
+        }
+      );
     } else {
       // Mark form controls as touched to display validation errors
       this.taskForm.markAllAsTouched();
     }
   }
-}
+
+  getTasks() {
+    this.http.get('http://localhost:3000/gettasks')
+      .subscribe(
+        (response: any) => {
+          if (response && response.message === 'success')
+            response.data = this.tasksList;
+            this.tasksList = response.data;
+          },
+        error => {
+          console.error('API Error:', error);
+        }
+      );
+  }
+}  
